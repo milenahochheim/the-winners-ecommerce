023b34774faa10a1701e289d006fc03320f0776b
Matheus Galvão <matheus.gs636@gmail.com>
2023-05-24 10:32:59 -0300

    commit local

--- src/main/java/com/dev/ecommerce/model/Endereco.java
+++ src/main/java/com/dev/ecommerce/model/Endereco.java
@@ -1,63 +1,44 @@
 package com.dev.ecommerce.model;
 
 import java.io.Serializable;
-import java.util.List;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-import javax.persistence.Transient;
 
 @Entity
-@Table(name = "cliente")
-public class Cliente implements Serializable {
+@Table(name = "endereco")
+public class Endereco implements Serializable{
 
     private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    private String nome;
 
-    private String cpf;
+    @ManyToOne
+    @JoinColumn(name = "cliente_id")
+    private Cliente cliente;
 
-    private String email;
-
-    private String senha;
-
-    private String nascimento;
-
-    private String genero;
-
-    @OneToMany(targetEntity = Endereco.class, cascade = CascadeType.ALL)
-    private List<Endereco> enderecos;
-    
-    @Transient
     private String cep;
 
-    @Transient
     private String rua;
 
-    @Transient
     private String numero;
 
-    @Transient
     private String bairro;
 
-    @Transient
     private String cidade;
 
-    @Transient
     private String uf;
 
-    @Transient
     private String complemento;
 
-    public Cliente() {
+    public Endereco() {
         super();
     }
 
@@ -67,54 +48,6 @@ public class Cliente implements Serializable {
 
     public void setId(Long id) {
         this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getSenha() {
-        return senha;
-    }
-
-    public void setSenha(String senha) {
-        this.senha = senha;
-    }
-
-    public String getNascimento() {
-        return nascimento;
-    }
-
-    public void setNascimento(String nascimento) {
-        this.nascimento = nascimento;
-    }
-
-    public String getGenero() {
-        return genero;
-    }
-
-    public void setGenero(String genero) {
-        this.genero = genero;
     }
 
     public String getCep() {
