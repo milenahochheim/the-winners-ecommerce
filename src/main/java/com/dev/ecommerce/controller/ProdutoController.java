2785ac91ed7cacc3eb95591dc62f0a37839b2a16
Matheus Galvão <matheus.gs636@gmail.com>
2023-05-26 00:52:10 -0300

    teste sem erro de syntaxe

--- src/main/java/com/dev/ecommerce/controller/ProdutoController.java
+++ src/main/java/com/dev/ecommerce/controller/ProdutoController.java
@@ -5,8 +5,6 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
--- src/test/java/com/dev/ecommerce/ProdutoControllerTest.java
+++ src/test/java/com/dev/ecommerce/ProdutoControllerTest.java
@@ -1,24 +1,33 @@
 package com.dev.ecommerce;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Optional;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.servlet.ModelAndView;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 
 import com.dev.ecommerce.controller.ProdutoController;
+import com.dev.ecommerce.model.Produto;
 import com.dev.ecommerce.repository.ProdutoRepository;
-
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Optional;
-
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
 
 class ProdutoControllerTest {
     private MockMvc mockMvc;
@@ -26,62 +35,55 @@ class ProdutoControllerTest {
     @Mock
     private ProdutoRepository produtoRepository;
 
+    @InjectMocks
     private ProdutoController produtoController;
 
     @BeforeEach
     void setUp() {
         MockitoAnnotations.initMocks(this);
-        produtoController = new ProdutoController();
-        // produtoController.setProdutoRepository(produtoRepository);
         mockMvc = MockMvcBuilders.standaloneSetup(produtoController).build();
     }
 
     @Test
-    void testRetornarImagem() throws Exception {
+    void testSalvar_ProdutoComArquivo() throws Exception {
+        MockMultipartFile file = new MockMultipartFile("file", "test.jpg", "image/jpeg", "test image data".getBytes());
+        Produto produto = new Produto();
+        produto.setId(1L);
+
+        when(produtoRepository.saveAndFlush(any(Produto.class))).thenReturn(produto);
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.multipart("/admin/produtos/salvar")
+        .file(file)
+        .param("nome", "Test Produto")
+        .param("descricao", "Test Description")
+        .param("preco", "10.00");
+        
+        mockMvc.perform(requestBuilder)
+        .andExpect(status().isOk())
+        .andExpect(view().name("admin/produtos/cadastro"))
+        .andExpect(model().attributeExists("produto"));
+                
+
+        verify(produtoRepository, times(1)).saveAndFlush(any(Produto.class));
+        verify(produtoRepository, times(1)).saveAndFlush(produto);
+        // Verifique outras ações necessárias, como a escrita do arquivo.
+    }
+
+    @Test
+    void testRetornarImagem_ImagemExistente() throws Exception {
+        String imageName = "existing_image.jpg";
         byte[] imageData = "test image data".getBytes();
         Path tempFile = Files.createTempFile("test", ".jpg");
         Files.write(tempFile, imageData);
 
-        String imageName = tempFile.getFileName().toString();
-
         when(produtoRepository.findById(any())).thenReturn(Optional.empty());
 
-        mockMvc.perform(get("/admin/produtos/mostrarImagem/{imagem}", imageName))
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/admin/produtos/mostrarImagem/{imagem}", imageName);
+
+        mockMvc.perform(requestBuilder)
                 .andExpect(status().isOk())
-                .andExpect(content().bytes(imageData));
+                .andExpect(content().bytes(imageData));//The method content() is undefined for the type ProdutoControllerTestJava(67108964)
 
         Files.deleteIfExists(tempFile);
-    }
-
-    @Test
-    void testRemover() throws Exception {
-        Long id = 1L;
-        Optional<Produto> produtoOptional = Optional.of(new Produto());
-        when(produtoRepository.findById(id)).thenReturn(produtoOptional);
-
-        mockMvc.perform(get("/admin/produtos/remover/{id}", id))
-                .andExpect(status().isOk())
-                .andExpect(view().name("listar"));
-
-        verify(produtoRepository, times(1)).delete(produtoOptional.get());
-    }
-
-    @Test
-    void testSalvar() throws Exception {
-        Produto produto = new Produto();
-        produto.setId(1L);
-
-        MockMultipartFile file = new MockMultipartFile("file", "test.jpg", "image/jpeg", "test image data".getBytes());
-
-        mockMvc.perform(fileUpload("/admin/produtos/salvar")
-                .file(file)
-                .param("nome", "Test Produto")
-                .param("descricao", "Test Description")
-                .param("preco", "10.00"))
-                .andExpect(status().isOk())
-                .andExpect(view().name("cadastrar"))
-                .andExpect(model().attributeExists("produto"));
-
-        verify(produtoRepository, times(1)).saveAndFlush(any(Produto.class));
     }
 }
